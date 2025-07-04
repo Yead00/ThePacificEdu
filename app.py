4d2e7eeb6b35c195a7d74d3cacc74ba11752ae36
Yead Ahmed <ahmedyead10@gmail.com>
2025-07-04 09:48:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def home():
         "message": "Welcome to ThePacificEdu - AI chatbot for underserved learners."
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
